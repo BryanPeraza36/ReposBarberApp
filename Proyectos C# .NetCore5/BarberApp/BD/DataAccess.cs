8ad3f8646e1b76ca1d23811c58a80380f3e7b65c
BryanPeraza36 <bperaza07@hotmail.com>
2023-10-17 20:03:55 -0600

    String de conexion creado y en appsettings.json

--- Proyectos C# .NetCore5/BarberApp/BD/DataAccess.cs
+++ Proyectos C# .NetCore5/BarberApp/BD/DataAccess.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace BD
 {
     public class DataAccess
     {
+        private readonly IConfiguration config;
+
+        public DataAccess(IConfiguration _Config)
+        {
+            config = _Config;
+        }
+
+        public SqlConnection DbConnection => new SqlConnection(
+            new SqlConnectionStringBuilder(config.GetConnectionString("Conn")).ConnectionString
+            );
+        
     }
 }
